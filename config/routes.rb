59af127c481bf4d2711ca920c8218cab566eb7c7
thaynsworth <thomas.haynsworth@gmail.com>
2014-12-04 12:03:17 -0500

    created sessions and user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions
 
   root 'users#index'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  get 'signup' => 'users#new'
 
 end
