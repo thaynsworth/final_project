2f79c5ff16055c10bc8a796b87b6624ff81573f1
thaynsworth <thomas.haynsworth@gmail.com>
2014-12-04 23:42:30 -0500

    added basic map connections

---
+++ app/assets/javascripts/crimes.js
@@ -0,0 +1,9 @@
+// var map,
+//     markers = [];
+
+function drawMap(){
+  L.mapbox.accessToken = 'pk.eyJ1IjoidGhheW5zd29ydGgiLCJhIjoiWThsS0FVbyJ9.Ra30ahPRIVNTX9FBGK-hLg';
+  map = L.mapbox.map('map', 'thaynsworth.kcl6gjeb', {
+    zoomControl: false
+  }).setView([40.738, -73.857], 12)
+}
--- app/views/crimes/index.json.jbuilder
+++ app/views/crimes/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@crimes) do |crime|
-  json.extract! crime, :id, :type, :coordinates, :year, :total
+  json.extract! crime, :id, :name, :coordinates, :year, :total
   json.url crime_url(crime, format: :json)
 end
