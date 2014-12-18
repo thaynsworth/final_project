a45263b01a88f86854735d73136779b72ef8803b
thaynsworth <thomas.haynsworth@gmail.com>
2014-12-18 12:17:36 -0500

    profile functioning and pulling information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @user = User.new
+    @current_user
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
+
   end
 
   # GET /users/new
